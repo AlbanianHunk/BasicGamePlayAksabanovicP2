fddd2154f0462475eeebadb319f0bd0aeaa4d289
AlbanianHunk <116110355+AlbanianHunk@users.noreply.github.com>
2023-03-20 09:35:09 -0700

    Finished challange 2
    recording both when i get home

--- BasicGamePlayAksabanovicP2/Assets/Scripts/PlayerStats.cs
+++ BasicGamePlayAksabanovicP2/Assets/Scripts/PlayerStats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    private int lives = 3;
+    private int lives = 1;
     private int score = 0;
 
     // Start is called before the first frame update
